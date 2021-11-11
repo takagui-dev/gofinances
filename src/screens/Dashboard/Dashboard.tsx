c89a8e2b7ac8579b04851e763f33a91f6d6593d0
Guilherme Takahashi <gui.yukio175@gmail.com>
2021-11-11 20:34:39 -0300

    style: import logout icon

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -9,6 +9,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  Icon,
 } from './styles';
 
 export function Dashboard() {
@@ -23,6 +24,8 @@ export function Dashboard() {
               <UserName>Taka</UserName>
             </User>
           </UserInfo>
+
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
